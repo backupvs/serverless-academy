b4e82ceb9d541cbf49de8ba113db920f6994f80d
backupvs <dmitryholubev@gmail.com>
2023-11-03 16:50:45 +0200

    Created custom error and global error handler

---
+++ 2_module_backend_development/03_json_storage/src/common/error-handler.middleware.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import HttpError from '../errors/HttpError.js';
+import httpCodes from '../errors/httpCodes.js';
+
+/**
+ * @param {Error | HttpError} err
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const errorHandler = (err, req, res, next) => {
+  let statusCode = httpCodes.InternalError;
+  let message = 'Internal Error';
+
+  if (err instanceof HttpError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  if (err instanceof SyntaxError) {
+    statusCode = httpCodes.BadRequest;
+    message = err.message;
+  }
+
+  if (statusCode === httpCodes.InternalError) console.error(err);
+
+  res.status(statusCode).json({ success: false, error: message });
+};
